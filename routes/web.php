1410d4f48b922dcb821b63c26def40007ef5435d
Alex SaM <alsam3637@gmail.com>
2025-09-10 10:43:09 -0400

    Create Admin middleware and admin routes

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->group(function () {
-    Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function () {
     Route::match(['get', 'post'],'login', 'AdminController@login')->name('login');
+    Route::group(['middleware' => ['admin']], function () {
+        Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
+    });
+
 });
 
 
